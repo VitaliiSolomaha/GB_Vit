034d89d97a749d386d466c0a2e869c7c8dfddb0f
Vitalii Solomaha <vitalii.solomaha@gmail.com>
2022-08-14 23:53:44 +0300

    hw01 ex01 done

--- homework01/ex001/Program.cs
+++ homework01/ex001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b =7;
+if (a<b)
+{
+   Console.WriteLine("a = "+ a + ", b = "+ b + ", max = "+ b); 
+}
+else
+{
+    Console.WriteLine("a = "+ a + ", b = "+ b + ", max = "+ a); 
+}
